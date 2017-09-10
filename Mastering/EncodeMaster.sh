5db3f0ee8143ec6bec9bd3da0b28fe5f6ff80e34
Keijiro Takahashi <keijiro@gmail.com>
2017-09-11 00:36:07 +0900

    Tweaks

--- Mastering/EncodeMaster.sh
+++ Mastering/EncodeMaster.sh
@@ -1,1 +1,2 @@
-ffmpeg.exe -i MasterCF.mov -c:v prores_ks -pix_fmt yuv422p10le -profile:v 3 -qscale:v 9 -acodec copy Master.mov
+ffmpeg.exe -i Master_Main_CF.mov -c:v prores_ks -pix_fmt yuv422p10le -profile:v 3 -qscale:v 9 -acodec copy Master_Main.mov
+ffmpeg.exe -i Master_Mask_CF.mov -c:v prores_ks -pix_fmt yuv422p10le -profile:v 3 -qscale:v 9 -acodec copy Master_Mask.mov
