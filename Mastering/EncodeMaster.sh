2cd4cd115417b640955502de7f306b21b22453d2
Keijiro Takahashi <keijiro@gmail.com>
2017-09-17 00:10:35 +0900

    Slightly changed the settings.

--- Mastering/EncodeMaster.sh
+++ Mastering/EncodeMaster.sh
@@ -1,2 +1,2 @@
-ffmpeg.exe -i Master_Main_CF.mov -c:v prores_ks -pix_fmt yuv422p10le -profile:v 3 -qscale:v 9 -acodec copy Master_Main.mov
-ffmpeg.exe -i Master_Mask_CF.mov -c:v prores_ks -pix_fmt yuv422p10le -profile:v 3 -qscale:v 9 -acodec copy Master_Mask.mov
+ffmpeg.exe -y -i Master_Main_CF.mov -c:v prores_ks -pix_fmt yuv422p10le -profile:v 3 -qscale:v 9 -acodec copy Master_Main.mov
+ffmpeg.exe -y -i Master_Mask_CF.mov -c:v prores_ks -pix_fmt yuv422p10le -profile:v 3 -qscale:v 9 -acodec copy Master_Mask.mov
